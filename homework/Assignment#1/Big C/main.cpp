57ea207896794bd62f8af7b737e79d09d37461de
Fang24601 <djbb10@live.com>
2016-01-05 11:50:21 -0800

    Signed-off-by: Fang24601 <djbb10@live.com>

---
+++ homework/Assignment#1/Big C/main.cpp
@@ -0,0 +1,40 @@
+/* 
+ 
+  Author: Daniel Desforges
+ 
+  Created on January 5, 2016, 11:41 AM
+  Purpose: Check out IDE
+ */
+
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins here
+int main(int argc, char** argv) {
+    // Declare and initialize variables
+    
+    //Input data
+    
+    //Calculate or map inputs to outputs
+    
+    //Output the results
+    cout<<"  XXX"<<endl;
+    cout<<" X    X"<<endl;
+    cout<<"X"<<endl;
+    cout<<"X"<<endl;
+    cout<<"X"<<endl;
+    cout<<"X"<<endl;
+    cout<<" X    X"<<endl;
+    cout<<"  XXX"<<endl;
+    //Exit stage right
+    return 0;
+}
+
